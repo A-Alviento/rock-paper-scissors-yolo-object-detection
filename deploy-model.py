8b12987e00dac16d0bb26b1b89bcb5421722274c
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-05-17 13:22:05 +0800

    cleaned up files

--- deploy-model.py
+++ deploy-model.py
@@ -1,6 +1,9 @@
 from ultralytics import YOLO
-from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
-model = YOLO(r"C:\Users\Adrian\yolo-test-env-1\runs\detect\train\weights\best.pt")
-model.predict(source="0", show=True, conf=0.5)
+# loads the trained model, selecting the best model trained 
+model = YOLO("./runs/detect/train/weights/best.pt")
+
+# predict on source 0 (webcam) with a confidence threshold of 50%
+# device="mps" is for apple silicon to utilize the gpu; ignore if not needed
+model.predict(source="0", show=True, conf=0.5, device="mps")
